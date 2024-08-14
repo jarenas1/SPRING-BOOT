3acdcc33c37a8cd95d857d184feb22b045db84e0
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-13 19:48:44 -0500

    helloW

--- helloWorld/helloWorld/src/main/java/com/riwi/helloWorld/HelloWorldApplication.java
+++ helloWorld/helloWorld/src/main/java/com/riwi/helloWorld/HelloWorldApplication.java
@@ -1,5 +1,7 @@
-package com.example.demo;
+package com.riwi.helloWorld;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 	@GetMapping("/hello")
