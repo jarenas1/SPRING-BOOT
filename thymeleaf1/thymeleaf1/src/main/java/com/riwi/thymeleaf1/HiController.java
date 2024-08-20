b843fbeb5db3b49cf03bcabef5644d21ed915e3a
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-20 04:49:09 -0500

    thymeleaf

--- thymeleaf1/thymeleaf1/src/main/java/com/riwi/thymeleaf1/HiController.java
+++ thymeleaf1/thymeleaf1/src/main/java/com/riwi/thymeleaf1/HiController.java
@@ -1,2 +1,25 @@
-package com.riwi.thymeleaf1;public class HiController {
+package com.riwi.thymeleaf1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller //INDICAMOS QUE ESTA CLASE ES UN CONTROLADOR(RECIBE PETICIONES DE LA VISTA Y DEVUELVE ALGO)
+public class HiController {
+
+    //METODO QUE NO TIENE PARAMETROS DE HTML DINAMICOS
+    //@GetMapping("/hi") //indicamos que es una peticion de get con la ruta tal
+    //public String hola(){
+        //return "hi" ; //este es un metodo que va a interactuar con hi.html ubicado en templates, y DEBE devolver
+                      //el nombre del html al que hace referencia, en este caso "hi"
+   // }
+
+    @GetMapping("/hi")
+    public String hi(@RequestParam(name = "name", required = false, defaultValue = "Juanchito") //DECIMOS QUE SE VA A RECIBIR UN PARAMETRO LLAMADO NAME
+                     String name, Model model){ //DECIMOS QUE EL METODO RECIBE UN NOMBRE Y UN MODELO
+        model.addAttribute("name", name); //LE ASIGNAMOS AL MODELO EL NAME
+        return "hi";
+
+    }
 }
